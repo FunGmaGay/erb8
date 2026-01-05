dcf6f0b8d6180f8e06a6cbabf70946e140f90ef2
FunGmaGay <magaystudying@gmail.com>
2026-01-05 12:19:38 +0800

    Step 8: Updates on LogOut

--- config/settings.py
+++ config/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 from dotenv import load_dotenv 
 import os
+from django.contrib.messages import constants as messages
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -49,7 +50,8 @@ DJANGO_APPS = [
 
 APPLICATION_APPS = ['pages.apps.PagesConfig',
                     'doctors.apps.DoctorsConfig',
-                    'listings.apps.ListingsConfig',]
+                    'listings.apps.ListingsConfig',
+                    'accounts.apps.AccountsConfig',]
 
 THIRD_PARTY_APPS = ["debug_toolbar",]
 
@@ -154,3 +156,7 @@ INTERNAL_IPS = [
     "127.0.0.1",
 ]
 
+MESSAGE_TAGS = {
+    messages.ERROR:'danger',
+    messages.SUCCESS:'success',
+}
--- static/js/main.js
+++ static/js/main.js
@@ -1,2 +1,8 @@
 const date = new Date();
 document.querySelector('.year').innerHTML = date.getFullYear();
+
+setTimeout(() => {
+    $("#message").fadeOut("slow");
+}, 3000); 
+//to set the time for the alert to disappear
+// # is to search for 'message'
